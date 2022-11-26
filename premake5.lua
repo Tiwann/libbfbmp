f8f5959fdf7821c1979a3030287743e93b18c4f3
Erwann Messoah <tiwannbeats@gmail.com>
2022-11-26 10:51:30 +0100

    added tinyfiledialogs project for examples

--- premake5.lua
+++ premake5.lua
@@ -15,16 +15,28 @@ project "writing"
 
     files {
         "%{wks.location}/include/bfbmp.h",
-        "%{prj.location}/main.c",
+        "%{prj.location}/writing_main.c",
     }
 
     includedirs {
-        "%{wks.location}/include"
+        "%{wks.location}/include",
+        "%{wks.location}/dependencies/tinyfiledialogs/include",
     }
+    
+    links "tinyfiledialogs"
 
     filter "system:windows"
         defines "_CRT_SECURE_NO_WARNINGS"
-    filter ""
+    
+    filter "configurations:Debug"
+        symbols "On"
+        optimize "Off"
+        runtime "Debug"
+            
+    filter "configurations:Release"
+        symbols "Off"
+        optimize "On"
+        runtime "Release"
     
 project "reading"
     location "examples/reading"
@@ -37,17 +49,64 @@ project "reading"
 
     files {
         "%{wks.location}/include/bfbmp.h",
-        "%{prj.location}/main.c",
+        "%{prj.location}/reading_main.c",
     }
+    
+    links "tinyfiledialogs"
 
     includedirs {
-        "%{wks.location}/include"
+        "%{wks.location}/include",
+        "%{wks.location}/dependencies/tinyfiledialogs/include",
     }
 
     filter "system:windows"
         defines "_CRT_SECURE_NO_WARNINGS"
-    filter ""
     
+    filter "configurations:Debug"
+            symbols "On"
+            optimize "Off"
+            runtime "Debug"
+            
+        filter "configurations:Release"
+            symbols "Off"
+            optimize "On"
+            runtime "Release"
+
+group "dependencies"
+project "tinyfiledialogs"
+    location "dependencies/tinyfiledialogs"
+    kind "StaticLib"
+    language "C"
+    debugdir "%{prj.location}"
+    targetdir "%{prj.location}/binaries"
+    objdir "%{prj.location}/intermediate"
+
+    files {
+        "%{prj.location}/include/tfd.h",
+        "%{prj.location}/src/tfd.c",
+    }
+    
+    includedirs {
+        "%{prj.location}/include",
+    }
+    
+    filter "system:windows"
+        defines "_CRT_SECURE_NO_WARNINGS"
+    
+    filter "configurations:Debug"
+        symbols "On"
+        optimize "Off"
+        runtime "Debug"
+        
+    filter "configurations:Release"
+        symbols "Off"
+        optimize "On"
+        runtime "Release"
+    
+    filter "system:windows"
+        links { "Comdlg32.lib", "Ole32.lib" }
+
+
 group "scripts"
 project "scripts"
     kind "None"
