0154ff56e950309e60188db4f7a876824bfddd99
Erwann Messoah <tiwannbeats@gmail.com>
2022-12-13 15:29:16 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,117 +1,185 @@
 workspace "libbfbmp"
-    architecture "x64"
-    startproject "reading"
-    configurations { "Debug", "Release" }
+	architecture "x64"
+	startproject "reading"
+	configurations { "Debug", "Release" }
 
 group "examples"
 project "writing"
-    location "examples/writing"
-    kind "ConsoleApp"
-    language "C"
-    cdialect "C17"
-    debugdir "%{prj.location}"
-    targetdir "%{prj.location}/binaries"
-    objdir "%{prj.location}/intermediate"
+	location "examples/writing"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	debugdir "%{prj.location}"
+	targetdir "%{prj.location}/binaries/%{cfg.buildcfg}"
+	objdir "%{prj.location}/intermediate/%{cfg.buildcfg}"
 
-    files {
-        "%{wks.location}/include/bfbmp.h",
-        "%{prj.location}/writing_main.c",
-    }
+	files {
+		"%{wks.location}/include/bfbmp.h",
+		"%{prj.location}/main.cpp",
+	}
 
-    includedirs {
-        "%{wks.location}/include",
-        "%{wks.location}/dependencies/tinyfiledialogs/include",
-    }
-    
-    links "tinyfiledialogs"
+	includedirs {
+		"%{wks.location}/include",
+		"%{wks.location}/dependencies/tinyfiledialogs/include",
+	}
+	
+	links "tinyfiledialogs"
 
-    filter "system:windows"
-        defines "_CRT_SECURE_NO_WARNINGS"
-    
-    filter "configurations:Debug"
-        symbols "On"
-        optimize "Off"
-        runtime "Debug"
-            
-    filter "configurations:Release"
-        symbols "Off"
-        optimize "On"
-        runtime "Release"
-    
+	filter "system:windows"
+		defines "_CRT_SECURE_NO_WARNINGS"
+	
+	filter "configurations:Debug"
+		symbols "On"
+		optimize "Off"
+		runtime "Debug"
+			
+	filter "configurations:Release"
+		symbols "Off"
+		optimize "On"
+		runtime "Release"
+	
 project "reading"
-    location "examples/reading"
-    kind "ConsoleApp"
-    language "C"
-    cdialect "C17"
-    debugdir "%{prj.location}"
-    targetdir "%{prj.location}/binaries"
-    objdir "%{prj.location}/intermediate"
+	location "examples/reading"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	debugdir "%{prj.location}"
+	targetdir "%{prj.location}/binaries/%{cfg.buildcfg}"
+	objdir "%{prj.location}/intermediate/%{cfg.buildcfg}"
 
-    files {
-        "%{wks.location}/include/bfbmp.h",
-        "%{prj.location}/reading_main.c",
-    }
-    
-    links "tinyfiledialogs"
+	files {
+		"%{wks.location}/include/bfbmp.h",
+		"%{prj.location}/main.cpp",
+	}
+	
+	links "tinyfiledialogs"
 
-    includedirs {
-        "%{wks.location}/include",
-        "%{wks.location}/dependencies/tinyfiledialogs/include",
-    }
+	includedirs {
+		"%{wks.location}/include",
+		"%{wks.location}/dependencies/tinyfiledialogs/include",
+	}
 
-    filter "system:windows"
-        defines "_CRT_SECURE_NO_WARNINGS"
-    
-    filter "configurations:Debug"
-            symbols "On"
-            optimize "Off"
-            runtime "Debug"
-            
-        filter "configurations:Release"
-            symbols "Off"
-            optimize "On"
-            runtime "Release"
+	filter "system:windows"
+		defines "_CRT_SECURE_NO_WARNINGS"
+	
+	filter "configurations:Debug"
+			symbols "On"
+			optimize "Off"
+			runtime "Debug"
+			
+		filter "configurations:Release"
+			symbols "Off"
+			optimize "On"
+			runtime "Release"
+
+project "dllbuild"
+location "examples/dllbuild"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++20"
+	debugdir "%{prj.location}"
+	targetdir "%{prj.location}/binaries/%{cfg.buildcfg}"
+	objdir "%{prj.location}/intermediate/%{cfg.buildcfg}"
+
+	files {
+		"%{prj.location}/main.cpp",
+		"%{wks.location}/include/bfbmp.h"
+	}
+
+	includedirs {
+		"%{wks.location}/include",
+	}
+
+	defines {
+		"BFBMP_BUILD_DLL",
+		"BFBMP_EXPORT",
+	}
+
+
+	filter "system:windows"
+		defines "_CRT_SECURE_NO_WARNINGS"
+	
+	filter "configurations:Debug"
+		symbols "On"
+		optimize "Off"
+		runtime "Debug"
+			
+	filter "configurations:Release"
+		symbols "Off"
+		optimize "On"
+		runtime "Release"
+
+
 
 group "dependencies"
 project "tinyfiledialogs"
-    location "dependencies/tinyfiledialogs"
-    kind "StaticLib"
-    language "C"
-    debugdir "%{prj.location}"
-    targetdir "%{prj.location}/binaries"
-    objdir "%{prj.location}/intermediate"
+	location "dependencies/tinyfiledialogs"
+	kind "StaticLib"
+	language "C"
+	debugdir "%{prj.location}"
+	targetdir "%{prj.location}/binaries/%{cfg.buildcfg}"
+	objdir "%{prj.location}/intermediate/%{cfg.buildcfg}"
 
-    files {
-        "%{prj.location}/include/tfd.h",
-        "%{prj.location}/src/tfd.c",
-    }
-    
-    includedirs {
-        "%{prj.location}/include",
-    }
-    
-    filter "system:windows"
-        defines "_CRT_SECURE_NO_WARNINGS"
-    
-    filter "configurations:Debug"
-        symbols "On"
-        optimize "Off"
-        runtime "Debug"
-        
-    filter "configurations:Release"
-        symbols "Off"
-        optimize "On"
-        runtime "Release"
-    
-    filter "system:windows"
-        links { "Comdlg32.lib", "Ole32.lib" }
+	files {
+		"%{prj.location}/include/tinyfiledialogs.h",
+		"%{prj.location}/src/tinyfiledialogs.c",
+	}
+	
+	includedirs {
+		"%{prj.location}/include",
+	}
+	
+	filter "system:windows"
+		defines "_CRT_SECURE_NO_WARNINGS"
+	
+	filter "configurations:Debug"
+		symbols "On"
+		optimize "Off"
+		runtime "Debug"
+		
+	filter "configurations:Release"
+		symbols "Off"
+		optimize "On"
+		runtime "Release"
+	
+	filter "system:windows"
+		links { "comdlg32.lib", "ole32.lib" }
 
 
 group "scripts"
 project "scripts"
-    kind "None"
-    
-    files {
-        "%{wks.location}/premake5.lua",
-        "%{wks.location}/build.bat",
-    }
+	kind "None"
+	
+	files {
+		"%{wks.location}/premake5.lua",
+		"%{wks.location}/build.bat",
+	}
+
+newaction {
+	trigger = "clean",
+	description = "clean all generated files",
+	execute = function()
+		folders = os.matchdirs("**/binaries")
+		for i in pairs(folders) do
+			os.rmdir(folders[i])
+		end
+
+		folders = os.matchdirs("**/intermediate")
+		for i in pairs(folders) do
+			os.rmdir(folders[i])
+		end
+
+		os.rmdir(".idea")
+		os.rmdir(".vs")
+		os.rmdir(".vscode")
+		os.remove("*.sln")
+		os.remove("**.vcxproj")
+		os.remove("**.vcxproj.filters")
+		os.remove("**.vcxproj.user")
+		os.remove("**.make")
+		os.remove("**Makefile")
+		os.remove("**.cmake")
+		os.remove("**.CMakeLists.txt")
+	end
+
+}
